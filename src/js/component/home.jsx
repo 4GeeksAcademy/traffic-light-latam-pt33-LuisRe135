e83e32e132309fef4894b73463a7898cda8b3950
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-09-21 01:08:10 +0000

    centrado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,18 +10,20 @@ const Home = () => {
 	const [color, setColor] = useState("")
 
 	return (
-		<div className="text-center bg-black flex-column pt-3" style={{maxWidth: "8em", height: "20em"}}>
-			 
-			<div className={"rounded-circle m-auto mt-2 "+ (color =="red" ? "clickedLight-red" : "") } onClick={() =>{setColor("red")}} style={{width: "5em", height:"5em", backgroundColor: 'red'}}>
-            
+		<div className="container d-flex justify-content-center m-5">
+			<div className="text-center bg-black flex-column p-3" style={{maxWidth: "8em", height: "20em"}}>
+				
+				<div className={"rounded-circle m-auto mt-2 "+ (color =="red" ? "clickedLight-red" : "") } onClick={() =>{setColor("red")}} style={{width: "5em", height:"5em", backgroundColor: 'red'}}>
+				
+				</div>
+				<div className={"rounded-circle m-auto mt-2 "+ (color =="yellow" ? "clickedLight-yellow" : "") } onClick={() =>{setColor("yellow")}} style={{width: "5em", height:"5em", backgroundColor: 'yellow'}}>
+				
+				</div>
+				<button className={"rounded-circle m-auto mt-2 "+ (color =="green" ? "clickedLight-green" : "") }  onClick={() =>{setColor("green")}} style={{width: "5em", height:"5em", backgroundColor: 'green'}}>
+				
+				</button>
+			
 			</div>
-			<div className={"rounded-circle m-auto mt-2 "+ (color =="yellow" ? "clickedLight-yellow" : "") } onClick={() =>{setColor("yellow")}} style={{width: "5em", height:"5em", backgroundColor: 'yellow'}}>
-            
-			</div>
-			<button className={"rounded-circle m-auto mt-2 "+ (color =="green" ? "clickedLight-green" : "") }  onClick={() =>{setColor("green")}} style={{width: "5em", height:"5em", backgroundColor: 'green'}}>
-            
-			</button>
-		
 		</div>
 	);
 };
